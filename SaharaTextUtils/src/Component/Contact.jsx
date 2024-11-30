3ca6c3860591d6ee038c9f8c39f67f89433a108e
sarharatextutils <sahar333pk@gmail.com>
2024-12-01 00:33:56 +0500

    change number

--- SaharaTextUtils/src/Component/Contact.jsx
+++ SaharaTextUtils/src/Component/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = (props) => {
     <div className='contact container' style={{color:props.color==="light"?"black":"white"}}>
       <div className=''><b>Name:</b> Saher</div>
       <div className=""><b>Education:</b>SE</div>
-      <div className=""><b>Contact:</b>+923156568440</div>
+      <div className=""><b>Contact:</b>+923156560</div>
     </div>
   )
 }
